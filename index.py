61596b9cd259614759addf1f14e81b4c39ad784a
Radu Suciu <radusuciu@gmail.com>
2015-03-05 10:57:10 -0800

    Convert indents to spaces and small fixes

--- index.py
+++ index.py
@@ -5,11 +5,7 @@ from os.path import join
 import config
 
 app = Flask(__name__)
-
-app.config.from_object(__name__)
 app.debug = True
-DEBUG = True
-# add this so that flask doesn't swallow error messages
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
 @app.route('/')
@@ -19,22 +15,18 @@ def index():
 @app.route('/upload/<path>')
 def upload(path):
 
-	raw_files = glob(join(config.PATH, path, '*.RAW'))
+    raw_files = glob(join(config.PATH, path, '*.RAW'))
 
-	if len(raw_files):
-		for item in raw_files:
-			print item
-			call(['readw', '-c', item])
-			call(['rawconverter', item, '--ms2', '--out_folder', join(config.PATH, path) ])
-	else:
-		return 'nope'
-
-	return 'hello'
-
+    if len(raw_files):
+        for item in raw_files:
+            print item
+            call(['readw', '-c', item])
+            call(['rawconverter', item, '--ms2', '--out_folder', join(config.PATH, path) ])
+    else:
+        return 'nope'
 
 # @app.route('/status/<path>')
 # def status(path):
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
