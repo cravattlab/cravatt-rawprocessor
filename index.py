b9e6803df5d6ea5c9f89a4d393ef5b16671cf618
Radu Suciu <radusuciu@gmail.com>
2015-03-06 14:19:24 -0800

    Add status checking ability

--- index.py
+++ index.py
@@ -1,12 +1,18 @@
 from flask import Flask
-from subprocess import call
+from subprocess import call, PIPE
 from glob import glob
-from os.path import join
+from os.path import join, split, splitext
+from distutils import spawn
+import psutil
 import config
+import shelve
+import json
 
 app = Flask(__name__)
 app.debug = True
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
+processes = {}
 
 @app.route('/')
 def index():
@@ -18,15 +24,59 @@ def upload(path):
     raw_files = glob(join(config.PATH, path, '*.RAW'))
 
     if len(raw_files):
+        rawconv_path = split(spawn.find_executable('rawconverter'))[0]
+        rawconv_out = join(config.PATH, path)
+
+        processes[path] = []
+
         for item in raw_files:
-            print item
-            call(['readw', '-c', item])
-            call(['rawconverter', item, '--ms2', '--out_folder', join(config.PATH, path) ])
+
+            readw_proc = psutil.Popen(['readw', '-c', item], stdout=PIPE)
+            rawc_proc = psutil.Popen(
+                ['rawconverter', item, '--ms2', '--out_folder', rawconv_out ],
+                cwd=rawconv_path, stdout=PIPE
+            )
+
+            filename = splitext(item)[0]
+
+            processes[path].extend(({
+                'filename': filename + '.mzXML',
+                'process': readw_proc,
+                'status': None
+            }, {
+                'filename': filename + '.ms2',
+                'process': rawc_proc,
+                'status': None
+            }))
     else:
         return 'nope'
 
-# @app.route('/status/<path>')
-# def status(path):
+    return 'wat'
+
+
+@app.route('/status/<path>')
+def status(path):
+    status = []
+
+
+    if path in processes:
+        for process in processes[path]:
+            # get status string while we can
+            try:
+                process['status'] = process['process'].status()
+            # when process is dead we read stdout
+            except psutil.NoSuchProcess:
+                try:
+                    process['status'] = process['process'].communicate()[0]
+                except Exception:
+                    pass
+
+        return json.dumps([ { k: v for k, v in x.items() if k != 'process' } for x in processes[path] ])
+    else:
+        return '%s not found' % path
+
+    return 'wat'
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
