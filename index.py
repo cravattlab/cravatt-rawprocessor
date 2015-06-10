666628abebe0bd5ab38a44794b235baa97f4d7cc
Radu Suciu <radusuciu@gmail.com>
2015-06-10 16:08:44 -0700

    Many changes to make more suitable for consumption

--- index.py
+++ index.py
@@ -1,13 +1,12 @@
-from flask import Flask
-from subprocess import call, PIPE
+from flask import Flask, jsonify, make_response, abort
+from subprocess import PIPE
 from glob import glob
-from os.path import join, split, splitext
+from os.path import join, split, splitext, basename
 from distutils import spawn
+from cors import crossdomain
 import psutil
 import config
-import shelve
-import json
-from cors import crossdomain
+import os
 
 app = Flask(__name__)
 app.debug = True
@@ -16,69 +15,124 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 processes = {}
 
 @app.route('/')
-@crossdomain(origin='*')
-def index():
-    return 'hello world'
+def index(): return ''
 
-@app.route('/upload/<path>')
-def upload(path):
+@app.route('/convert/<path>')
+@crossdomain(origin='*')
+def convert(path):
+    # make sure we're not already running an operation for given path
+    if path in processes: abort(409)
 
     raw_files = glob(join(config.PATH, path, '*.RAW'))
+    # make sure the given path contains raw files
+    if not len(raw_files): abort(400)
 
-    if len(raw_files):
-        rawconv_path = split(spawn.find_executable('rawconverter'))[0]
-        rawconv_out = join(config.PATH, path)
+    rawconv_path = split(spawn.find_executable('rawconverter'))[0]
+    rawconv_out = join(config.PATH, path)
 
-        processes[path] = []
+    processes[path] = []
 
-        for item in raw_files:
+    for item in raw_files:
 
-            readw_proc = psutil.Popen(['readw', '-c', item], stdout=PIPE)
-            rawc_proc = psutil.Popen(
-                ['rawconverter', item, '--ms2', '--select_mono_prec', '--out_folder', rawconv_out ],
-                cwd=rawconv_path, stdout=PIPE
-            )
+        readw_proc = psutil.Popen(['readw', '-c', item], stdout=PIPE)
+        rawc_proc = psutil.Popen(
+            ['rawconverter', item, '--ms2', '--select_mono_prec', '--out_folder', rawconv_out ],
+            cwd=rawconv_path, stdout=PIPE
+        )
 
-            filename = splitext(item)[0]
+        filename = splitext(item)[0]
 
-            processes[path].extend(({
-                'filename': filename + '.mzXML',
-                'process': readw_proc,
-                'status': None
-            }, {
-                'filename': filename + '.ms2',
-                'process': rawc_proc,
-                'status': None
-            }))
-    else:
-        return 'nope'
+        processes[path].extend(({
+            'filename': basename(filename + '.mzXML'),
+            'process': readw_proc,
+            'status': None
+        }, {
+            'filename': basename(filename + '.ms2'),
+            'process': rawc_proc,
+            'status': None
+        }))
 
-    return 'wat'
-
+    return success_response('Requested files are being converted')
 
 @app.route('/status/<path>')
+@crossdomain(origin='*')
 def status(path):
-    status = []
+    if not path in processes: abort(404)
 
+    result = {
+        'progress': 0.0,
+        'status': 'running'
+    }
+    
+    for process in processes[path]:
+        # get status string while we can
+        try:
+            process['status'] = process['process'].status()
 
-    if path in processes:
-        for process in processes[path]:
-            # get status string while we can
+        # when process is dead we read from stdout
+        except psutil.NoSuchProcess:
+            result['progress'] = result['progress'] + 1
+
             try:
-                process['status'] = process['process'].status()
-            # when process is dead we read stdout
-            except psutil.NoSuchProcess:
-                try:
-                    process['status'] = process['process'].communicate()[0]
-                except Exception:
-                    pass
+                return_code = process['process'].poll()
+                # process['output'] = process['process'].communicate()[0]
+                if return_code == 0:
+                    process['status'] = 'success'
+                else:
+                    process['status'] = 'fail'
+            # exception triggered when we try and poll a process we've already polled
+            except Exception:
+                pass
 
-        return json.dumps([ { k: v for k, v in x.items() if k != 'process' } for x in processes[path] ], sort_keys=True, indent=4, separators=(',', ': '))
-    else:
-        return '%s not found' % path
+        if process['status'] == 'fail':
+            if 'fail' in result['status']:
+                result['status']['fail'].append(process['filename'])
+            else:
+                result['status'] = {
+                    'fail': [ process['filename'] ]
+                }
 
-    return 'wat'
+    # crude estimator of progress
+    result['progress'] = round(result['progress']/len(processes[path]), 2) * 100
 
+    if result['progress'] == 100:
+        if not 'fail' in result['status']:
+            result['status'] = 'success'
+        del processes[path]
+
+    return jsonify(result)
+
+@app.route('/abort/<path>')
+@crossdomain(origin='*')
+def abort_conversion(path):
+    if not path in processes: abort(404)
+
+    for process in processes[path]:
+        try:
+            process['process'].kill()
+        except Exception:
+            pass
+
+    del processes[path]
+    return success_response('Conversion successfully aborted')
+
+def error_response(error, code):
+    return make_response(jsonify({ 'error': error }), code)
+
+def success_response(message):
+    return jsonify({ 'success' : message })
+
+@app.errorhandler(400)
+def erroneous_path(error):
+    return error_response('No .raw files found at specified path', 400)
+
+@app.errorhandler(404)
+def status_failed(error):
+    return error_response('There is no conversion operation for the given path', 404)
+
+@app.errorhandler(409)
+def conversion_running(error):
+    return error_response('There is already a conversion operation running for the given path', 409)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
